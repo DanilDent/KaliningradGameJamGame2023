73d7a1683f7a4c236aa8e2b84e033bf10de17233
Danil Dent <farctate11@gmail.com>
2023-08-05 13:07:24 +0400

    [Drawing_Dead] Add movement and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,45 +2,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController _controller;
-    private Vector3 _playerVelocity;
+    private Rigidbody _rigidbody;
+    private Vector3 _movement;
+    [SerializeField] private float _playerSpeed;
+    [SerializeField] private float _acceleration;
+    [SerializeField] private float _deceleration;
+    [SerializeField] private float _jumpForce;
     private bool _isGrounded;
-    [SerializeField] private float _playerSpeed = 2.0f;
-    [SerializeField] private float _jumpHeight = 1.0f;
-    [SerializeField] private float _gravityValue = -9.81f;
 
-    private void Start()
+    private void Awake()
     {
-        _controller = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
-        _isGrounded = _controller.isGrounded;
-        if (_isGrounded && _playerVelocity.y < 0)
-        {
-            _playerVelocity.y = 0f;
-        }
-
-        Vector3 move = new Vector3(0f, 0f, Input.GetAxis("Vertical"));
-
-        _controller.Move(move * Time.deltaTime * _playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        UpdateGrounded();
+        HandleMovement();
 
 
-        Debug.Log($"isGrounded: {_isGrounded}");
-
-        // Changes the height position of the player..
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
         }
 
-        _playerVelocity.y += _gravityValue * Time.deltaTime;
-        _controller.Move(_playerVelocity * Time.deltaTime);
+        Debug.Log($"Is grounded: {_isGrounded}");
+    }
+
+    private void HandleMovement()
+    {
+        if (!_isGrounded)
+        {
+            return;
+        }
+
+        _movement = new Vector3(0f, 0f, Input.GetAxis("Horizontal"));
+        _rigidbody.AddForce(_movement * _acceleration * Time.deltaTime, ForceMode.Acceleration);
+        if (_rigidbody.velocity.magnitude > _playerSpeed)
+        {
+            _rigidbody.velocity = _rigidbody.velocity.normalized * _playerSpeed;
+        }
+    }
+
+    private void UpdateGrounded()
+    {
+        Debug.DrawRay(transform.position + Vector3.up * 0.025f, -Vector3.up * 0.05f, Color.red);
+        int groundLayerMask = LayerMask.GetMask("Ground");
+        if (Physics.Raycast(transform.position + Vector3.up * 0.025f, -Vector3.up, out var hitInfo, 0.05f, groundLayerMask))
+        {
+            _isGrounded = true;
+            Debug.Log(hitInfo.collider.gameObject.layer);
+        }
+        else
+        {
+            _isGrounded = false;
+        }
     }
 }
