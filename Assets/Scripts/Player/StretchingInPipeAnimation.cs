6353d39b9ae4de02be73c7b848f6317a7514a9ba
Danil Dent <farctate11@gmail.com>
2023-08-06 15:05:19 +0400

    WIP save commit

---
+++ Assets/Scripts/Player/StretchingInPipeAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StretchingInPipeAnimation : MonoSingleton<StretchingInPipeAnimation>
+{
+    [SerializeField] private Transform _firstBone;
+    private PlayerSO _config;
+
+    private void Start()
+    {
+        _config = PlayerSettings.Instance.Config;
+    }
+
+    public void Stretch()
+    {
+        _firstBone.transform.position += _firstBone.transform.forward * _config.CrouchInPipeSpeed * Time.deltaTime;
+    }
+}
